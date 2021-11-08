f43c1b0ebc2def79e90db8edaad9e1c00ca633ef
Winston Leung <wleung85@uwo.ca>
2021-11-07 20:16:01 -0500

    Implemented addGif function

--- sola/tests/sola.js
+++ sola/tests/sola.js
@@ -1,13 +1,42 @@
 const anchor = require('@project-serum/anchor');
 
+// The SystemProgram that basically runs Solana
+const { SystemProgram } = anchor.web3;
+
 const main = async() => {
   console.log("🚀 Starting test...")
 
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
   const program = anchor.workspace.Sola;
-  const tx = await program.rpc.startStuffOff();
+
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Call start_stuff_off, pass it the params it needs!
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log("📝 Your transaction signature", tx);
+
+  // Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString());
+
+  await program.rpc.addGif({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+  
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString());
 }
 
 const runMain = async () => {
